676331c31fccb31e57c351351e93b9722502cc6b
arnaudlagarde <56533282+arnaudlagarde@users.noreply.github.com>
2022-02-22 14:54:25 +0100

    Add Risk Entity

---
+++ migrations/Version20220222135314.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220222135314 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE risk (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, identification_date DATETIME NOT NULL, resolution_date DATETIME NOT NULL, critical TINYINT(1) NOT NULL, probability VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE risk');
+        $this->addSql('ALTER TABLE admin CHANGE username username VARCHAR(180) NOT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE roles roles LONGTEXT NOT NULL COLLATE `utf8mb4_unicode_ci` COMMENT \'(DC2Type:json)\', CHANGE password password VARCHAR(255) NOT NULL COLLATE `utf8mb4_unicode_ci`');
+        $this->addSql('ALTER TABLE projet CHANGE title title VARCHAR(255) NOT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE description description LONGTEXT DEFAULT NULL COLLATE `utf8mb4_unicode_ci`');
+        $this->addSql('ALTER TABLE status CHANGE name name VARCHAR(255) NOT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE slug slug VARCHAR(255) NOT NULL COLLATE `utf8mb4_unicode_ci`');
+        $this->addSql('ALTER TABLE user CHANGE firstname firstname VARCHAR(255) NOT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE lastname lastname VARCHAR(255) NOT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE email email VARCHAR(255) NOT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE password password VARCHAR(255) NOT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE role role VARCHAR(255) NOT NULL COLLATE `utf8mb4_unicode_ci`');
+    }
+}
---
+++ src/Entity/Risk.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\RiskRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: RiskRepository::class)]
+class Risk
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $Name;
+
+    #[ORM\Column(type: 'datetime')]
+    private $IdentificationDate;
+
+    #[ORM\Column(type: 'datetime')]
+    private $ResolutionDate;
+
+    #[ORM\Column(type: 'boolean')]
+    private $Critical;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private $Probability;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->Name;
+    }
+
+    public function setName(string $Name): self
+    {
+        $this->Name = $Name;
+
+        return $this;
+    }
+
+    public function getIdentificationDate(): ?\DateTimeInterface
+    {
+        return $this->IdentificationDate;
+    }
+
+    public function setIdentificationDate(\DateTimeInterface $IdentificationDate): self
+    {
+        $this->IdentificationDate = $IdentificationDate;
+
+        return $this;
+    }
+
+    public function getResolutionDate(): ?\DateTimeInterface
+    {
+        return $this->ResolutionDate;
+    }
+
+    public function setResolutionDate(\DateTimeInterface $ResolutionDate): self
+    {
+        $this->ResolutionDate = $ResolutionDate;
+
+        return $this;
+    }
+
+    public function getCritical(): ?bool
+    {
+        return $this->Critical;
+    }
+
+    public function setCritical(bool $Critical): self
+    {
+        $this->Critical = $Critical;
+
+        return $this;
+    }
+
+    public function getProbability(): ?string
+    {
+        return $this->Probability;
+    }
+
+    public function setProbability(?string $Probability): self
+    {
+        $this->Probability = $Probability;
+
+        return $this;
+    }
+}
