e338006add0b264ba7858077de58138d0d6831bc
arnaudlagarde <56533282+arnaudlagarde@users.noreply.github.com>
2022-02-22 15:18:22 +0100

    Budget Entity added

---
+++ migrations/Version20220222141757.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220222141757 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE budget (id INT AUTO_INCREMENT NOT NULL, initial_value DOUBLE PRECISION NOT NULL, consumed_value DOUBLE PRECISION NOT NULL, remaining_budget DOUBLE PRECISION NOT NULL, turnover_budget VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE budget');
+        $this->addSql('ALTER TABLE admin CHANGE username username VARCHAR(180) NOT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE roles roles LONGTEXT NOT NULL COLLATE `utf8mb4_unicode_ci` COMMENT \'(DC2Type:json)\', CHANGE password password VARCHAR(255) NOT NULL COLLATE `utf8mb4_unicode_ci`');
+        $this->addSql('ALTER TABLE crucial_fact CHANGE name name VARCHAR(255) NOT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE description description LONGTEXT DEFAULT NULL COLLATE `utf8mb4_unicode_ci`');
+        $this->addSql('ALTER TABLE fact CHANGE name name VARCHAR(255) NOT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE description description VARCHAR(255) NOT NULL COLLATE `utf8mb4_unicode_ci`');
+        $this->addSql('ALTER TABLE portfolio CHANGE name name VARCHAR(255) NOT NULL COLLATE `utf8mb4_unicode_ci`');
+        $this->addSql('ALTER TABLE projet CHANGE title title VARCHAR(255) NOT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE description description LONGTEXT DEFAULT NULL COLLATE `utf8mb4_unicode_ci`');
+        $this->addSql('ALTER TABLE risk CHANGE name name VARCHAR(255) NOT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE probability probability VARCHAR(255) DEFAULT NULL COLLATE `utf8mb4_unicode_ci`');
+        $this->addSql('ALTER TABLE status CHANGE name name VARCHAR(255) NOT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE slug slug VARCHAR(255) NOT NULL COLLATE `utf8mb4_unicode_ci`');
+        $this->addSql('ALTER TABLE tag CHANGE name name VARCHAR(255) NOT NULL COLLATE `utf8mb4_unicode_ci`');
+        $this->addSql('ALTER TABLE user CHANGE firstname firstname VARCHAR(255) NOT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE lastname lastname VARCHAR(255) NOT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE email email VARCHAR(255) NOT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE password password VARCHAR(255) NOT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE role role VARCHAR(255) NOT NULL COLLATE `utf8mb4_unicode_ci`');
+    }
+}
---
+++ src/Entity/Budget.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\BudgetRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: BudgetRepository::class)]
+class Budget
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'float')]
+    private $InitialValue;
+
+    #[ORM\Column(type: 'float')]
+    private $ConsumedValue;
+
+    #[ORM\Column(type: 'float')]
+    private $RemainingBudget;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $TurnoverBudget;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getInitialValue(): ?float
+    {
+        return $this->InitialValue;
+    }
+
+    public function setInitialValue(float $InitialValue): self
+    {
+        $this->InitialValue = $InitialValue;
+
+        return $this;
+    }
+
+    public function getConsumedValue(): ?float
+    {
+        return $this->ConsumedValue;
+    }
+
+    public function setConsumedValue(float $ConsumedValue): self
+    {
+        $this->ConsumedValue = $ConsumedValue;
+
+        return $this;
+    }
+
+    public function getRemainingBudget(): ?float
+    {
+        return $this->RemainingBudget;
+    }
+
+    public function setRemainingBudget(float $RemainingBudget): self
+    {
+        $this->RemainingBudget = $RemainingBudget;
+
+        return $this;
+    }
+
+    public function getTurnoverBudget(): ?string
+    {
+        return $this->TurnoverBudget;
+    }
+
+    public function setTurnoverBudget(string $TurnoverBudget): self
+    {
+        $this->TurnoverBudget = $TurnoverBudget;
+
+        return $this;
+    }
+}
