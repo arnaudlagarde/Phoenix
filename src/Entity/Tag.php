b3eb4a5d4b04585753cdb55ce50eb97004498792
arnaudlagarde <56533282+arnaudlagarde@users.noreply.github.com>
2022-02-22 15:03:06 +0100

    Tag entity added

---
+++ src/Entity/Tag.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TagRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: TagRepository::class)]
+class Tag
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $name;
+
+    #[ORM\Column(type: 'integer')]
+    private $value;
+
+    #[ORM\Column(type: 'boolean')]
+    private $Mandatory;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getValue(): ?int
+    {
+        return $this->value;
+    }
+
+    public function setValue(int $value): self
+    {
+        $this->value = $value;
+
+        return $this;
+    }
+
+    public function getMandatory(): ?bool
+    {
+        return $this->Mandatory;
+    }
+
+    public function setMandatory(bool $Mandatory): self
+    {
+        $this->Mandatory = $Mandatory;
+
+        return $this;
+    }
+}
