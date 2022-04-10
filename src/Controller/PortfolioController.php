71b03e85ad16222f2a7d22bc259ecc5a755c27a7
arnaudlagarde <xx-momo@hotmail.fr>
2022-04-10 17:57:40 +0200

    wip team & portfolio controllers

--- src/Controller/PortfolioController.php
+++ src/Controller/PortfolioController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Portfolio;
+use App\Repository\PortfolioRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +11,17 @@ use Symfony\Component\Routing\Annotation\Route;
 class PortfolioController extends AbstractController
 {
     #[Route('/portfolio', name: 'app_portfolio')]
-    public function index(): Response
+    public function index(PortfolioRepository $portfolioRepository): Response
     {
         return $this->render('portfolio/index.html.twig', [
-            'controller_name' => 'PortfolioController',
+            'portfolios' => $portfolioRepository->findAll(),
+        ]);
+    }
+    #[Route('/portfolio/{id}', name: 'app_show_portfolio')]
+    public function show(Portfolio $portfolio, $id): Response
+    {
+        return $this->render('portfolio/show.html.twig', [
+            'portfolio' => $portfolio,
         ]);
     }
 }
--- src/Controller/TeamController.php
+++ src/Controller/TeamController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Team;
+use App\Repository\TeamRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +11,17 @@ use Symfony\Component\Routing\Annotation\Route;
 class TeamController extends AbstractController
 {
     #[Route('/team', name: 'app_team')]
-    public function index(): Response
+    public function index(TeamRepository $teamRepository): Response
     {
         return $this->render('team/index.html.twig', [
-            'controller_name' => 'TeamController',
+            'teams' => $teamRepository->findAll(),
+        ]);
+    }
+    #[Route('/team/{id}', name: 'app_show_team')]
+    public function show(Team $team): Response
+    {
+        return $this->render('team/show.html.twig', [
+            'team' => $team,
         ]);
     }
 }
